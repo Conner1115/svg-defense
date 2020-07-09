c62ee6bff7e577b35568663d0a9476c7ac1aae86
Conner Ow <connerow1115@gmail.com>
2020-07-09 17:33:39 -0500

    Update DOMhelpers.js

--- DOMhelpers.js
+++ DOMhelpers.js
@@ -1,3 +1,33 @@
-var $ = function (prop) {
+var $ = function(prop){
   return document.querySelector(prop);
 };
+var mouseX,mouseY,mouseMoved = false, clicked = false, mouseOver = true, mouseIsPressed = false, mouseDragged = false,mouseOut = false;
+var frameRate = 100;
+var draw = function(){};
+$("body").addEventListener("mousemove",function(e){
+  mouseX = e.x;
+  mouseY = e.y;
+  mouseMoved = true;
+});
+$("body").addEventListener("click",function(){
+  clicked = true;
+  setTimeout(function(){clicked = false},1000/frameRate)
+})
+$("body").addEventListener("mouseover",function(){
+  mouseOver = true;
+  mouseOut = false;
+})
+$("body").addEventListener("mouseout",function(){
+  mouseOver = false;
+  mouseOut = true;
+})
+$("body").addEventListener("mousedown",function(){
+  mouseIsPressed = true;
+})
+$("body").addEventListener("mouseup",function(){
+  mouseIsPressed = false;
+})
+$("body").addEventListener("drag",function(){
+  mouseDragged = true;
+})
+setInterval(draw,1000/frameRate);
